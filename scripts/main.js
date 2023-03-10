cb874df9b574f813d95502bbcf3f0f27cf6f73aa
Iris Vonberg <irisavonberg@gmail.com>
2023-03-09 17:45:53 -0800

    js for buttons on Settings page & settings html updates

---
+++ scripts/main.js
@@ -0,0 +1,49 @@
+const searchBtn = document.querySelector('#search');
+const breedSelect = document.querySelector('#breedSelect');
+const removeBreed = document.querySelector('#removeBreed');
+let chosenBreeds = document.querySelector('#chosenBreeds');
+let breedSelected = document.querySelector('#searchBox').value;
+let breedPreview = document.querySelector('#breedPreview');
+
+
+let breeds = [];
+
+
+searchBtn.addEventListener('click', getResults)
+
+function getResults(e) {
+    e.preventDefault();
+    breedPreview.innerHTML = "";
+    let breedSelected = document.querySelector('#searchBox').value.toLowerCase();
+    let byBreedUrl = 'https://dog.ceo/api/breed/'+ breedSelected +'/images/random/4';
+    fetch(byBreedUrl).then(function(res) {
+        return res.json();
+    }).then(function(breedSelected) {
+        breedSelected.message.forEach(function(picture) {
+            let previewPic = document.createElement('img');
+            previewPic.setAttribute('src', picture);
+            previewPic.style.display = "inline-block";
+            previewPic.style.width = "25%";
+            previewPic.style.height = "200px";
+            breedPreview.appendChild(previewPic);
+        })
+    })
+}
+
+
+breedSelect.addEventListener('click', addBreed);
+
+function addBreed() {
+    let breedSelected = document.querySelector('#searchBox').value;
+    breeds.push(breedSelected);
+    chosenBreeds.innerHTML = `${breeds}, `;
+    console.log(breeds);
+}
+
+removeBreed.addEventListener('click', clearBreeds)
+
+function clearBreeds() {
+    breeds = [];
+    console.log(breeds);
+    chosenBreeds.innerHTML = "No breeds yet :("
+};
