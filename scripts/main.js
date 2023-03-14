def4ab830b1899f745b3768e1e5da21f75cf9d03
Iris Vonberg <irisavonberg@gmail.com>
2023-03-14 14:53:02 -0700

    fixed search bug & added error handling for same breed

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ let breedSelected = document.querySelector('#searchBox').value;
 let breedPreview = document.querySelector('#breedPreview');
 
 savedBreeds = window.localStorage.getItem('breeds');
-console.log(savedBreeds)
 if (savedBreeds != " ") {
     chosenBreeds.innerHTML = `${savedBreeds}, `;
     removeBreed.classList.remove('hidden');
@@ -27,14 +26,15 @@ function getResults(e) {
     fetch(byBreedUrl).then(function(res) {
             return res.json();
     }).then(function(breedSelected) {
-        console.log(breedSelected);
         if(breedSelected.status == 'success'){
-        breedSelect.classList.remove('hidden');        breedSelected.message.forEach(function(picture) {
+            breedPreview.innerHTML = " "
+            breedSelected.message.forEach(function(picture) {
             let previewPic = document.createElement('img');
             previewPic.setAttribute('src', picture);
             previewPic.style.width = "25%";
             previewPic.style.height = "400px";
             breedPreview.appendChild(previewPic);
+            breedSelect.classList.remove('hidden');
         })
         } else {
             breedSelect.classList.add('hidden');
@@ -50,10 +50,14 @@ function addBreed() {
     let breedSelected = document.querySelector('#searchBox').value;
     let breeds = localStorage.getItem('breeds');
     breeds = breeds ? breeds.split(','): [];
+    console.log(breeds.includes(breedSelected));
+    if(breeds.includes(breedSelected) === true){
+        alert("Breed already selected")
+    } else {
     breeds.push(breedSelected.toLowerCase());
     localStorage.setItem('breeds', breeds.toString());
     chosenBreeds.innerHTML = `${breeds}`;
-    removeBreed.classList.remove('hidden');}
+    removeBreed.classList.remove('hidden');}}
 
 removeBreed.addEventListener('click', clearBreeds)
 
